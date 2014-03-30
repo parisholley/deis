f7cb7277329782a98471ad1e155cea04e132b836
Gabriel Monroy <gabriel@opdemand.com>
2014-03-30 13:52:44 -0600

    add sidekick container for logging, unit cleanup

--- controller/scheduler/coreos.py
+++ controller/scheduler/coreos.py
@@ -32,47 +32,74 @@ class FleetClient(object):
         """
         Setup a CoreOS cluster including router and log aggregator
         """
-        self._create_router()
-        self._create_logger()
+        print 'Creating deis-router'
+        self._create_router('deis-router', 'deis/router', command='', port=80)
+        print 'Creating deis-logger'
+        self._create_logger('deis-logger', 'deis/logger', command='', port=514)
 
-    def _create_router(self, name='deis-router', image='deis/router'):
-        self.create(name, image, template=ROUTER_TEMPLATE)
-        self.start(name)
+    def _create_router(self, name, image, command, port):
+        env = self.env.copy()
+        self._create_container(name, image, command, ROUTER_TEMPLATE, env, port)
+        self._create_announcer(name, image, command, ANNOUNCE_TEMPLATE, env, port)
+        self._start_container(name, env)
+        self._start_announcer(name, env)
 
-    def _create_logger(self, name='deis-logger', image='deis/logger'):
-        self.create(name, image, template=LOGGER_TEMPLATE)
-        self.start(name)
+    def _create_logger(self, name, image, command, port):
+        env = self.env.copy()
+        self._create_container(name, image, command, LOGGER_TEMPLATE, env, port)
+        self._create_announcer(name, image, command, ANNOUNCE_TEMPLATE, env, port)
+        self._start_container(name, env)
+        self._start_announcer(name, env)
+
+    def _destroy_router(self, name, env):
+        env = self.env.copy()
+        self._destroy_container(name, env)
+        self._destroy_announcer(name, env)
+
+    def _destroy_logger(self, name, env):
+        self._destroy_container(name, env)
+        self._destroy_announcer(name, env)
 
     def tearDown(self):
         """
         Tear down a CoreOS cluster including router and log aggregator
         """
-        self.destroy('deis-router')
-        self.destroy('deis-logger')
+        env = self.env.copy()
+        print 'Destroying deis-router'
+        self._destroy_router('deis-router', env)
+        print 'Destroying deis-logger'
+        self._destroy_logger('deis-logger', env)
 
     # job api
 
-    def create(self, name, image, command='', template=None):
+    def create(self, name, image, command='', template=None, port=5000):
         """
         Create a new job
         """
         print 'Creating {name}'.format(**locals())
         env = self.env.copy()
-        self._create_container(name, image, command, env)
-        self._create_announcer(name, image, command, env)
+        self._create_container(name, image, command, template or CONTAINER_TEMPLATE, env, port)
+        self._create_log(name, image, command, LOG_TEMPLATE, env, port)
+        self._create_announcer(name, image, command, ANNOUNCE_TEMPLATE, env, port)
 
     # TODO: remove hardcoded ports
 
-    def _create_container(self, name, image, command, env, port=5000):
+    def _create_container(self, name, image, command, template, env, port):
         env.update({'FLEETW_UNIT': name + '.service'})
-        env.update({'FLEETW_UNIT_DATA': base64.b64encode(CONTAINER_TEMPLATE.format(**locals()))})
+        env.update({'FLEETW_UNIT_DATA': base64.b64encode(template.format(**locals()))})
         return subprocess.check_call('fleetctl.sh submit {name}.service'.format(**locals()),
                                      shell=True, env=env)
 
-    def _create_announcer(self, name, image, command, env, port=5000):
+    def _create_announcer(self, name, image, command, template, env, port):
         env.update({'FLEETW_UNIT': name + '-announce' + '.service'})
-        env.update({'FLEETW_UNIT_DATA': base64.b64encode(ANNOUNCE_TEMPLATE.format(**locals()))})
+        env.update({'FLEETW_UNIT_DATA': base64.b64encode(template.format(**locals()))})
         return subprocess.check_call('fleetctl.sh submit {name}-announce.service'.format(**locals()),  # noqa
+                                     shell=True, env=env)
+
+    def _create_log(self, name, image, command, template, env, port):
+        env.update({'FLEETW_UNIT': name + '-log' + '.service'})
+        env.update({'FLEETW_UNIT_DATA': base64.b64encode(template.format(**locals()))})
+        return subprocess.check_call('fleetctl.sh submit {name}-log.service'.format(**locals()),  # noqa
                                      shell=True, env=env)
 
     def start(self, name):
@@ -81,6 +108,7 @@ class FleetClient(object):
         """
         print 'Starting {name}'.format(**locals())
         env = self.env.copy()
+        self._start_log(name, env)
         self._start_container(name, env)
         self._start_announcer(name, env)
 
@@ -94,6 +122,11 @@ class FleetClient(object):
             'fleetctl.sh start {name}-announce.service'.format(**locals()),
             shell=True, env=env)
 
+    def _start_log(self, name, env):
+        return subprocess.check_call(
+            'fleetctl.sh start {name}-log.service'.format(**locals()),
+            shell=True, env=env)
+
     def stop(self, name):
         """
         Stop a running job
@@ -102,6 +135,7 @@ class FleetClient(object):
         env = self.env.copy()
         self._stop_announcer(name, env)
         self._stop_container(name, env)
+        self._stop_log(name, env)
 
     def _stop_container(self, name, env):
         return subprocess.check_call(
@@ -109,6 +143,11 @@ class FleetClient(object):
             shell=True, env=env)
 
     def _stop_announcer(self, name, env):
+        return subprocess.check_call(
+            'fleetctl.sh stop {name}-announce.service'.format(**locals()),
+            shell=True, env=env)
+
+    def _stop_log(self, name, env):
         return subprocess.check_call(
             'fleetctl.sh stop {name}-announce.service'.format(**locals()),
             shell=True, env=env)
@@ -121,6 +160,7 @@ class FleetClient(object):
         env = self.env.copy()
         self._destroy_announcer(name, env)
         self._destroy_container(name, env)
+        self._destroy_log(name, env)
 
     def _destroy_container(self, name, env):
         return subprocess.check_call(
@@ -130,6 +170,11 @@ class FleetClient(object):
     def _destroy_announcer(self, name, env):
         return subprocess.check_call(
             'fleetctl.sh destroy {name}-announce.service'.format(**locals()),
+            shell=True, env=env)
+
+    def _destroy_log(self, name, env):
+        return subprocess.check_call(
+            'fleetctl.sh destroy {name}-log.service'.format(**locals()),
             shell=True, env=env)
 
     def run(self, name, image, command):
@@ -159,31 +204,9 @@ After=docker.service
 Requires=docker.service
 
 [Service]
-ExecStartPre=/bin/sh -c "/usr/bin/docker pull {image}"  # noqa
-ExecStart=/bin/bash -c '/usr/bin/docker run --name {name} -P -e PORT={port} -e ETCD=172.17.42.1:4001 --rm {image} {command} | logger -p local0.info -t {name} --tcp --server $(etcdctl get /deis/logger/host) --port $(etcdctl get /deis/logger/port)'  # noqa
-ExecStop=/bin/bash -c '/usr/bin/docker stop {name} || /usr/bin/docker rm {name}'
-"""
-
-ROUTER_TEMPLATE = """
-[Unit]
-Description=Deis Router
-After=docker.service
-Requires=docker.service
-
-[Service]
-ExecStart=/bin/bash -c '/usr/bin/docker run --name {name} -P -e ETCD=172.17.42.1:4001 {image} {command}'  # noqa
-ExecStop=/usr/bin/docker stop {name}
-"""
-
-LOGGER_TEMPLATE = """
-[Unit]
-Description=Deis Logger
-After=docker.service
-Requires=docker.service
-
-[Service]
-ExecStart=/bin/bash -c '/usr/bin/docker run --name {name} -P -e ETCD=172.17.42.1:4001 {image} {command}'  # noqa
-ExecStop=/usr/bin/docker stop {name}
+ExecStartPre=/usr/bin/docker pull {image}
+ExecStart=-/usr/bin/docker run --name {name} -P -e PORT={port} -e ETCD=172.17.42.1:4001 {image} {command}
+ExecStop=-/usr/bin/docker rm -f {name}
 """
 
 ANNOUNCE_TEMPLATE = """
@@ -192,10 +215,48 @@ Description={name} announce
 BindsTo={name}.service
 
 [Service]
-ExecStartPre=/bin/sh -c "echo Waiting for TCP port; until /usr/bin/docker port {name} {port} >/dev/null 2>&1; do sleep 2; done; port=$(docker port {name} {port} | cut -d ':' -f2); until cat </dev/null>/dev/tcp/%H/$port; do sleep 1; done"  # noqa
-ExecStart=/bin/sh -c "port=$(docker port {name} {port} | cut -d ':' -f2); while netstat -lnt | grep $port >/dev/null; do etcdctl set /deis/services/{name} %H:$port --ttl 60; sleep 45; done"  # noqa
+ExecStartPre=/bin/sh -c "until /usr/bin/docker port {name} {port} >/dev/null 2>&1; do sleep 2; done; port=$(docker port {name} {port} | cut -d ':' -f2); echo Waiting for $port/tcp...; until cat </dev/null>/dev/tcp/%H/$port; do sleep 1; done"
+ExecStart=/bin/sh -c "port=$(docker port {name} {port} | cut -d ':' -f2); echo Connected to $port/tcp, publishing to etcd...; while netstat -lnt | grep $port >/dev/null; do etcdctl set /deis/services/{name} %H:$port --ttl 60; sleep 45; done"
 ExecStop=/usr/bin/etcdctl rm --recursive /deis/services/{name}
 
 [X-Fleet]
 X-ConditionMachineOf={name}.service
+"""
+
+LOG_TEMPLATE = """
+[Unit]
+Description={name} log
+BindsTo={name}.service
+
+[Service]
+ExecStartPre=/bin/sh -c "until /usr/bin/docker inspect {name} >/dev/null 2>&1; do sleep 1; done"
+ExecStart=/bin/sh -c "/usr/bin/docker logs -f {name} 2>&1 | logger -p local0.info -t {name} --tcp --server $(etcdctl get /deis/services/deis-logger | cut -d ':' -f1) --port $(etcdctl get /deis/services/deis-logger | cut -d ':' -f2)"
+
+[X-Fleet]
+X-ConditionMachineOf={name}.service
+"""
+
+ROUTER_TEMPLATE = """
+[Unit]
+Description={name} router
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStartPre=/usr/bin/docker pull {image}
+ExecStart=-/usr/bin/docker run --name {name} -P -e ETCD=172.17.42.1:4001 {image} {command}
+ExecStop=-/usr/bin/docker rm -f {name}
+"""
+
+
+LOGGER_TEMPLATE = """
+[Unit]
+Description={name} logger
+After=docker.service
+Requires=docker.service
+
+[Service]
+ExecStartPre=/usr/bin/docker pull {image}
+ExecStart=-/usr/bin/docker run --name {name} -p 514:514 -e HOST=%H -e PORT=514 -e ETCD=172.17.42.1:4001 {image} {command}
+ExecStop=-/usr/bin/docker rm -f {name}
 """
