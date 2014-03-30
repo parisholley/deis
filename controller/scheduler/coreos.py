20d4f54f993a5711cfdfd8a0d32a59618d0adcbc
Gabriel Monroy <gabriel@opdemand.com>
2014-03-29 19:46:21 -0600

    new `deis run` implementation and celery task

--- controller/scheduler/coreos.py
+++ controller/scheduler/coreos.py
@@ -92,6 +92,17 @@ class FleetClient(object):
             'fleetctl.sh destroy {name}.service'.format(**locals()),
             shell=True, env=self.env)
 
+    def run(self, name, image, command):
+        """
+        Run a one-off command
+        """
+        print 'Running {name}'.format(**locals())
+        output = subprocess.PIPE
+        p = subprocess.Popen('fleetrun.sh {command}'.format(**locals()), shell=True, env=self.env,
+                             stdout=output, stderr=subprocess.STDOUT)
+        rc = p.wait()
+        return rc, p.stdout.read()
+
     def attach(self, name):
         """
         Attach to a job's stdin, stdout and stderr
